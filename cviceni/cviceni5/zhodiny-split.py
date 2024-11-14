556910c6e37b9415cecf5898e7de40d236215346
KobyStu <Kobylj01@ef.jcu.cz>
2024-11-14 13:43:56 +0000

    oprava odsazeni

--- cviceni/cviceni5/zhodiny-split.py
+++ cviceni/cviceni5/zhodiny-split.py
@@ -11,18 +11,18 @@ def read_data(file_name):
 def write_data(file_name,data):
    with open(file_name,"w") as file:
       
-if __name__ == "__main__":
-   try:
-    file = sys.argv[1]
-    data = read_data(file)
-    print(data)
-    text = "Alice,Bob,Tomas"
-    split_text = text.split(',')
-    print(split_text)
-   except FileNotFoundError:
-       print("Chyba: Soubor nebyl nalezen/neexistuje.")
-   except IndexError:
-       print("Chyba: Nebyly zadány soubory")
+    if __name__ == "__main__":
+        try:
+            file = sys.argv[1]
+            data = read_data(file)
+            print(data)
+            text = "Alice,Bob,Tomas"
+            split_text = text.split(',')
+            print(split_text)
+        except FileNotFoundError:
+            print("Chyba: Soubor nebyl nalezen/neexistuje.")
+        except IndexError:
+            print("Chyba: Nebyly zadány soubory")
     
 
     #otevrit soubor, cist po řadčich, rozseknout pomoci splitu a dat dohromady
